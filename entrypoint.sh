5008f355e413491641fee78ac73647d505bd100b
kof99zip <131327941+kof99zip@users.noreply.github.com>
2026-01-23 00:21:27 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ echo 'PermitRootLogin no' > /etc/ssh/sshd_config.d/my_sshd.conf
 
 echo "Service Starting..."
 nohup /usr/bin/cloudflared --no-autoupdate tunnel run --token $CLOUDFLARED_TOKEN > /dev/null 2>&1 &
-nohup /ssh/ttyd -6 -p 7681 -c kof97zip:kof97boss -W bash 1>/dev/null 2>&1 &
+nohup /ssh/ttyd -6 -p 7860 -c kof97zip:kof97boss -W bash 1>/dev/null 2>&1 &
 nohup /usr/sbin/php-fpm8.1 --nodaemonize --fpm-config /etc/php/8.1/fpm/php-fpm.conf > /dev/null 2>&1 &
 nginx
 nohup /usr/local/x-ui/x-ui > /dev/null 2>&1 &
